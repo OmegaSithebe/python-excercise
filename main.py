8825b039675cbdfdf5579e53199f049bb37762b5
OmegaSithebe <rogersithebe@gmail.com>
2025-12-08 10:35:17 +0200

    08/12/2025 home commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from python-exercise!")
+
+
+if __name__ == "__main__":
+    main()
+    
+ 
---
+++ python_1/datatype-05-12.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+x = 10
+y = 20
+
+print(x + y)
+
+#data type (int, float, str, bool)
+
+x = 30
+print(type(x))  #int
+print(id(x))    #memory address
+
+y = 40.5
+print(type(y))  #float
+print(id(y))    #memory address
+
