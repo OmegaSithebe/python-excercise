a111080d24d8360cc5a96eedd59797f7cfb73d9a
Omega Sithebe <omega.sithebe@sabiogroup.com>
2026-04-28 13:27:50 +0200

    working with multi function intro

---
+++ ai_engineer_course/multiple_functions.py
@@ -0,0 +1,16 @@
+# Now we will extend this concept by learning how to:
+# - Use **multiple functions in a single API call**
+# - Allow the model to **choose or call multiple functions**
+# - Control which function the model should call
+# This capability is called **Parallel Function Calling**.
+
+# **Parallel Function Calling** means that the AI model can:
+# - Access **multiple functions**
+# - Decide **which function(s) to call**
+# - Return **multiple function responses**
+
+# It improves communication between the **application and the model** by allowing the model to:
+# - Process more complex tasks
+# - Use multiple tools
+# - Generate richer responses
+
